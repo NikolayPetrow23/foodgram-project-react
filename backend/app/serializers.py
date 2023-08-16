b248c20f6ca7054a80ddd5811547bde2ac81d645
NikolayPetro23 <nikolaypetrow13@mail.ru>
2023-08-16 14:19:19 +0700

    Deploy project

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -148,7 +148,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         ingredients_data = validated_data.pop('ingredients')
-        validated_data.pop('tags')
+        # validated_data.pop('tags')
         recipe = Recipe.objects.create(**validated_data)
 
         ingredient_objs = [
